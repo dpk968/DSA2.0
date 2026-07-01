6c6fe3e934c6d9cf864c2846b9e6435481232182
Deepak Gupta <deepak@Deepaks-MacBook-Air.local>
2026-07-01 22:19:13 +0530

    phase one

---
+++ binarySearch/phase1/LB.java
@@ -0,0 +1,38 @@
+package binarySearch.phase1;
+
+import java.util.Scanner;
+
+public class LB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {3,5,8,15,19};
+
+        int x = 20;
+
+        int lb = getLB(arr,arr.length,x);
+        System.out.println("Lb-> "+x+" is: "+lb);
+
+        sc.close();
+    }
+
+    public static int getLB(int[] arr, int n, int x) {
+        int ans = n;
+
+        int l=0,h=n-1;
+
+        while (l<=h) {
+            int mid = (l+h)/2;
+
+            if(arr[mid]>=x){
+                h = mid-1;
+                ans = mid;
+            }else{
+                l=mid+1;
+            }
+        }
+
+
+        return ans;
+    }
+}
