6bd2dec2e963539a388764b924f982658d5460cb
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2026-08-02 20:12:10 +0530

    Time: 7 ms (21.79%) | Memory: 46.6 MB (21.03%) - LeetSync

---
+++ leetcode/909-stone-game/stone-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+
+        int l=0,r=piles.length-1;
+        int[][] dp = new int[r+1][r+1];
+        int diff = getTotalPilesDiff(piles,l,r,dp);
+        return diff>=0;
+    }
+
+    public int getTotalPilesDiff(int[] piles, int s, int e,int[][] dp){
+        if(s==e) return piles[s];
+
+        if(dp[s][e]!=0) return dp[s][e];
+        int left = piles[s] - getTotalPilesDiff(piles,s+1,e,dp);
+        int right = piles[e] - getTotalPilesDiff(piles,s,e-1,dp);
+
+        return dp[s][e]=Math.max(left,right);
+    }
+}
