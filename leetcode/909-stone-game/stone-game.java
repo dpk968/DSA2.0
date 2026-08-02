3a7b5b58407081e12608a6d083bb7f276f81e04b
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2026-08-02 20:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (48.30%) - LeetSync

--- leetcode/909-stone-game/stone-game.java
+++ leetcode/909-stone-game/stone-game.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         return true;
+    }
+
+
+
+
+
+
         // int l=0,r=piles.length-1;
         // int[][] dp = new int[r+1][r+1];
         // int diff = getTotalPilesDiff(piles,l,r,dp);
         // return diff>=0;
-    }
+    // }
 
     public int getTotalPilesDiff(int[] piles, int s, int e,int[][] dp){
         if(s==e) return piles[s];
