eb6a24380a61c6f82b4c82370eb39b714f37b5f9
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2026-08-03 20:23:50 +0530

    Time: 207 ms (5.44%) | Memory: 48.1 MB (35.88%) - LeetSync

---
+++ leetcode/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumPushes(String word) {
+        
+        
+        
+        Map<Character,Integer> map = new TreeMap<>();
+        
+        int keyPress = 0;
+        for(char ch:word.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        TreeMap<Character, Integer> sortedmap = new TreeMap<>((k1, k2) -> {
+            int cmp = map.get(k2).compareTo(map.get(k1));
+            return (cmp == 0) ? k2.compareTo(k1) : cmp; // tie-break by key
+        });
+        sortedmap.putAll(map);
+        int cnt = 0;
+        for(char key:sortedmap.keySet()){
+            keyPress += ((cnt++/8)+1)*sortedmap.get(key);
+        }
+
+        return keyPress;
+    }
+}
