9c8b5651d7ecbd4be1ea1171a88819fdffaa170d
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2026-08-09 12:21:30 +0530

    Time: 32 ms (86.84%) | Memory: 136 MB (97.37%) - LeetSync

--- leetcode/3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ leetcode/3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,98 +1,51 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
 
-        char[] c1 = word1.toCharArray();
-        char[] c2 = word2.toCharArray();
+        int m = word1.length();
+        int n = word2.length();
 
-        int n = c1.length;
-        int m = c2.length;
 
-        /*
-         * dp[i] = number of characters from the END of word2
-         * that can be matched exactly using word1[i...n-1].
-         */
-        int[] dp = new int[n + 1];
+        int[] rightMatchedString = new int[m];
 
-        int j = m - 1;
+        int i=m-1;
+        int j=n-1;
+        int charMatched = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-
-            if (j >= 0 && c1[i] == c2[j]) {
-                dp[i] = dp[i + 1] + 1;
+        while (i>=0&&j>=0) {
+            if(word1.charAt(i) == word2.charAt(j)){
+                charMatched++;
                 j--;
-            } else {
-                dp[i] = dp[i + 1];
             }
+            rightMatchedString[i] = charMatched;
+            i--;
+        }
+        while(i>=0){
+            rightMatchedString[i] = charMatched;
+            i--;
         }
 
-        int[] ans = new int[m];
-
-        int i = 0;
-        j = 0;
-
-        /*
-         * Greedily select the smallest possible index.
-         */
-        while (i < n && j < m) {
-
-            // Exact match
-            if (c1[i] == c2[j]) {
-
-                ans[j] = i;
+        i=0;
+        j=0;
+        int idx = 0;
+        int[] seq = new int[n];
+        boolean canChange = true;
+        while (i<m&&j<n) {
+            if(word1.charAt(i) == word2.charAt(j)){
+                seq[idx] = i;
                 j++;
-
-            } else {
-
-                /*
-                 * Use our one allowed mismatch here.
-                 *
-                 * After using i for word2[j], we need
-                 * m-j-1 characters to be matched exactly.
-                 *
-                 * dp[i+1] tells us how many characters
-                 * from the end can be matched.
-                 */
-                if (dp[i + 1] >= m - 1 - j) {
-
-                    ans[j] = i;
-                    j++;
-
-                    // Mismatch is used.
-                    i++;
-
-                    break;
-                }
+                idx++;
+            }else if(canChange && i+1<m && rightMatchedString[i+1] >= n-j-1){
+                canChange = false;
+                seq[idx] = i;
+                idx++;
+                j++;
             }
-
             i++;
         }
 
-        /*
-         * If we couldn't select all characters,
-         * no valid answer exists.
-         */
-        if (j < m && i == n) {
-            return new int[0];
+        if (j!=n) {
+            seq = new int[0];
         }
-
-        /*
-         * Now the mismatch has already been used,
-         * so we can only select exact matches.
-         */
-        while (j < m && i < n) {
-
-            if (c1[i] == c2[j]) {
-                ans[j] = i;
-                j++;
-            }
-
-            i++;
-        }
-
-        if (j < m) {
-            return new int[0];
-        }
-
-        return ans;
+        return seq;
     }
 }
